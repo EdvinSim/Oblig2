d083ce82e6651e971e90e2d4678fcbf381dfd8af
EdvinSim <edvin0106@hotmail.com>
2022-10-12 15:05:27 +0200

    renamed Innlevering2 to Main

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+
+    //___MAIN___
+    public static void main(String[] args) {
+        
+        String filename = args[0];
+
+        SorterParent[] sorters = {new Bubble(), new Heap(), new Merge(), new MyInsertion(), new MyQuick(), new Selection()};
+
+        System.out.println(SortAnalyzer.header());
+
+        for(SorterParent s: sorters) SortAnalyzer.analyze(filename, s);
+    }
+}
